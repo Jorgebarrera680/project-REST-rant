1ad01ea1c72cd98745b15c6e890251452ff18838
jorge <jorgebarrera680@gmail.com>
2022-09-29 22:28:34 -0500

    Adding 404 page view

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    res.render('places/index')
 })
+
+
+// GET /places
+
 
 module.exports = router
---
+++ views/places/index.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+const Def = require('./default')
+
+function index () {
+    return (
+      <Def>
+          <main>
+              <h1> hello ;/</h1>
+              <p>Oops, sorry, we can't find this page!</p>
+          </main>
+      </Def>
+    )
+  }
+  
+
+module.exports = error404
