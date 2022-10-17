83791ebdaa79531cd05f305db87d459220e52c36
jorge <jorgebarrera680@gmail.com>
2022-10-16 22:30:31 -0500

    done

--- controllers/places.js
+++ controllers/places.js
@@ -1,129 +1,53 @@
 const router = require('express').Router()
-const places = require('../models/places.js')
+const db = require('../models')
+const places = require('../models/places')
 
 router.get('/', (req, res) => {
-   
-res.render('places/show', { place: places[id] })
+    db.Place.find()
+    .then((places) => {
+      res.prependOnceListener('places/index',{places})
+
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('error404')
+    })
+})
+
+
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then()
+  .catch()
+})
+
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
 })
 
 router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
-})
-
-// More code ...
-
-
-
-router.get('/', (req, res) => {
-    let places = [{
-      name: 'H-Thai-ML',
-      city: 'Seattle',
-      state: 'WA',
-      cuisines: 'Thai, Pan-Asian',
-      pic: '/desktop/sad-dog.jpg'
-    }, {
-      name: 'Coding Cat Cafe',
-      city: 'Phoenix',
-      state: 'AZ',
-      cuisines: 'Coffee, Bakery',
-      pic: 'http://placekitten.com/250/250'
-    }]
-    
-    res.render('places/index', { places })
-  
-  
-    //res.render('places/index')
-  })
-// GET /places
-
-function index (data) {
-  let placesFormatted = data.places.map((place) => {
-    return index})}
-  
-
-router.post('/', (req, res) => {
-  
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-
-  places.push(req.body)
-  res.redirect('/places')
-})
-
-
-router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
-})
-
-
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] })
-  }
+  res.send('GET /places/:id stub')
 })
 
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
-
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
+  res.send('PUT /places/:id stub')
 })
 
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
 
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
 
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
 
 module.exports = router
--- models/places.js
+++ models/places.js
@@ -11,3 +11,16 @@ module.exports = [{
     cuisines: 'Coffee, Bakery',
     pic: '/images/coffee-cat.jpg'
 }]
+ 
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
