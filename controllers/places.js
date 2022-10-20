3c84aa2677cd54c349b723b01782444507945340
jorge <jorgebarrera680@gmail.com>
2022-10-20 00:23:26 -0500

    done

--- controllers/places.js
+++ controllers/places.js
@@ -2,17 +2,43 @@ const router = require('express').Router()
 const db = require('../models')
 const places = require('../models/places')
 
-router.get('/', (req, res) => {
-    db.Place.find()
-    .then((places) => {
-      res.prependOnceListener('places/index',{places})
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
 
-    })
-    .catch(err => {
-      console.log(err)
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
       res.render('error404')
-    })
+  })
 })
+
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+      }
+      console.log('Validation error message', message)
+      res.render('places/new', { message })
+  }
+  else {
+      res.render('error404')
+  }
+})
+})
+
 
 
 router.post('/', (req, res) => {
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -29,6 +29,16 @@ function new_form (data) {
     <input className="form-control" id="cuisines" name="cuisines" required />
   </div>
   <input className="btn btn-primary" type="submit" value="Add Place" />
+  <div className="form-group col-sm-4">
+  <label htmlFor="founded">Founded Year</label>
+  <input 
+    type="number" 
+    className="form-control" 
+    id="founded" 
+    name="founded" 
+    value={new Date().getFullYear()} />
+</div>
+
 </form>
 
             
